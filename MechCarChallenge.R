c01b8476410508ed462ca6f1205e9ad7d6e5b755
tomstowell99 <102548266+tomstowell99@users.noreply.github.com>
2022-07-16 12:00:15 -0500

    Delete MechCarChallenge.R

--- MechCarChallenge.R
+++
@@ -1,5 +0,0 @@
-library(dplyr)
-MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-multi = lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=MechaCar)
-summary(multi)
-
